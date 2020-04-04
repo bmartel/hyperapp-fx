3dfbf9180558271b0df979572bc9e8d8c866aed7
Brandon Martel <brandonmartel@gmail.com>
2020-04-04 08:35:13 -0500

    no const

--- src/fx/Graphql.js
+++ src/fx/Graphql.js
@@ -1,25 +1,22 @@
 import { Http } from "./Http.js"
 
-export const Graphql = (props = {}) => ({
-  url,
-  query,
-  variables,
-  options = {},
-  ...httpProps
-} = {}) =>
-  Http({
-    ...props,
-    ...httpProps,
-    url: url || props.url,
-    options: {
-      method: "POST",
-      body: JSON.stringify(variables ? { query, variables } : { query }),
-      ...(props.options || {}),
-      ...options,
-      headers: {
-        "content-type": "application/json",
-        ...((props.options || {}).headers || {}),
-        ...(options.headers || {})
+export function Graphql(props = {}) {
+  return function({ url, query, variables, options = {}, ...httpProps } = {}) {
+    return Http({
+      ...props,
+      ...httpProps,
+      url: url || props.url,
+      options: {
+        method: "POST",
+        body: JSON.stringify(variables ? { query, variables } : { query }),
+        ...(props.options || {}),
+        ...options,
+        headers: {
+          "content-type": "application/json",
+          ...((props.options || {}).headers || {}),
+          ...(options.headers || {})
+        }
       }
-    }
-  })
+    })
+  }
+}
